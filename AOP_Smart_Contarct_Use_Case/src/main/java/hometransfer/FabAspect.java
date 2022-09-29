d56e10c3b4a8ef0fb06a9b58e0a2ed17e9c214fd
imransuet <imransuet@gmail.com>
2022-09-29 13:53:17 +0200

    sample use case added

---
+++ AOP_Smart_Contarct_Use_Case/src/main/java/hometransfer/FabAspect.java
@@ -0,0 +1,38 @@
+package hometransfer;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Aspect
+public class FabAspect {
+
+
+    Logger log = LoggerFactory.getLogger(FabAspect.class);
+    @Pointcut("execution(* hometransfer.HomeTransfer.*(..))")
+    public void myPointcut() {
+    }
+
+    @Around("myPointcut()")
+    public Object applicationLogger(ProceedingJoinPoint pjp) throws Throwable {
+
+        //ProceedingJoinPoint is an extension of the JoinPoint which provides reflective access to the state available at a given join point, like method parameters, return value, or thrown exception. It also provides all static information about the method itself.
+        ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+      // mapper.configure(SerializationFeature.FAIL_ON_SELF_REFERENCES, false);
+        String methodName = pjp.getSignature().getName(); //this will give us the method name
+        String className = pjp.getTarget().getClass().toString(); // this will give us the class name
+        Object[] array = pjp.getArgs(); //this will capture the incoming argument to the method
+
+
+        log.info("Entering the " + className);
+        log.info("Entering the method "  + methodName + "()");
+        // log.info("Entering the method "  + methodName + "()" + " with arguments : " +mapper.writeValueAsString(array));
+        Object object = pjp.proceed(); // after calling this proceed method, we can capture the response
+        log.info(" Leaving the method : " + methodName + "()");
+        log.info(" Leaving the method : " + methodName + "()" + " with following Response : " + mapper.writeValueAsString(object));
+        return object;
+    }
+}
---
+++ AOP_Smart_Contarct_Use_Case/src/main/java/hometransfer/Home.java
@@ -0,0 +1,77 @@
+package hometransfer;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.owlike.genson.annotation.JsonProperty;
+import org.hyperledger.fabric.contract.annotation.DataType;
+import org.hyperledger.fabric.contract.annotation.Property;
+import java.util.Objects;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@DataType()  //This isa Class level annotation, indicating this class represents one of the complex types that can be returned or passed to the transaction functions.
+public final class Home {
+    @Property() // Field and parameter level annotation defining a property of the class (identified by DataType) Can also be used on the parameters of transaction functions
+    private final String id;
+    @Property()
+    private final String name;
+    @Property()
+    private final String area;
+    @Property()
+    private final String owner;
+    @Property()
+    private final String value;
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getArea() {
+        return area;
+    }
+    public String getOwner() {
+        return owner;
+    }
+    public String getValue() {
+        return value;
+    }
+
+    public Home(@JsonProperty("id") final String id, @JsonProperty("name") final String name,
+                @JsonProperty("area") final String area, @JsonProperty("owner") final String owner,
+                @JsonProperty("value") final String value) {
+        this.id = id;
+        this.name = name;
+        this.area = area;
+        this.owner = owner;
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if ((obj == null) || (getClass() != obj.getClass())) {
+            return false;
+        }
+
+        Home other = (Home) obj;
+
+        return Objects.deepEquals(new String[] { getId(), getName(), getArea(), getOwner(), getValue() },
+                new String[] { other.getId(), other.getName(), other.getArea(), other.getOwner(), other.getValue() });
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getArea(), getOwner(), getValue());
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + "@" + Integer.toHexString(hashCode()) + " [id=" + id + ", name=" + name
+                + ", area=" + area + ", owner=" + owner + ", value=" + value + "]";
+    }
+
+    //These three auto generated methods(equals, hashCode and toString) are for converting  message to JSON format and vice versa
+
+}
