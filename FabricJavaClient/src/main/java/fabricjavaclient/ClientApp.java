3c24ee7ce1e534601b3ed2cd26ad5bc4259060a5
imransuet <imransuet@gmail.com>
2023-04-19 12:52:19 +0200

    added TransientData  handling Capacity to Client API

--- FabricJavaClient/src/main/java/fabricjavaclient/ClientApp.java
+++ FabricJavaClient/src/main/java/fabricjavaclient/ClientApp.java
@@ -1,51 +1,65 @@
 package fabricjavaclient;
 
+
+
+import org.hyperledger.fabric.gateway.*;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
-import org.hyperledger.fabric.gateway.Contract;
-import org.hyperledger.fabric.gateway.Gateway;
-import org.hyperledger.fabric.gateway.Network;
-import org.hyperledger.fabric.gateway.Wallet;
-import org.hyperledger.fabric.gateway.Wallets;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ClientApp {
-	
-	static {
-		System.setProperty("org.hyperledger.fabric.sdk.service_discovery.as_localhost", "true");
-	}
-	
-	public static void main(String[] args) throws Exception {
-		// Load a file system based wallet for managing identities.
-		Path walletPath = Paths.get("wallet");
-		Wallet wallet = Wallets.newFileSystemWallet(walletPath);
-		// load a CCP
-		Path networkConfigPath = Paths.get("/home","saif", "fabric-samples", "test-network", "organizations", "peerOrganizations", "org1.example.com", "connection-org1.yaml");
 
-		Gateway.Builder builder = Gateway.createBuilder();
-		builder.identity(wallet, "appUser").networkConfig(networkConfigPath).discovery(true);
+    static {
+        System.setProperty("org.hyperledger.fabric.sdk.service_discovery.as_localhost", "true");
+    }
 
-		// create a gateway connection
-		try (Gateway gateway = builder.connect()) {
+    public static void main(String[] args) throws Exception {
+        // Load a file system based wallet for managing identities.
+        Path walletPath = Paths.get("wallet");
+        Wallet wallet = Wallets.newFileSystemWallet(walletPath);
+        // load a Connection Profile
+        Path networkConfigPath = Paths.get("/home", "saif", "fabric-samples", "test-network", "organizations", "peerOrganizations", "org1.example.com", "connection-org1.yaml");
 
-			// get the network and contract
-			Network network = gateway.getNetwork("samplechannel");
-			Contract contract = network.getContract("HomeTransfer");
+        Gateway.Builder builder = Gateway.createBuilder();
+        builder.identity(wallet, "appUser").networkConfig(networkConfigPath).discovery(true);
 
-			byte[] result;
+        // create a gateway connection
+        try (Gateway gateway = builder.connect()) {
 
-			contract.submitTransaction("addNewHome", "4", "Home4", "54678", "Grey", "78909");
+            // get the network and contract
+            Network network = gateway.getNetwork("samplechannel");
+            Contract contract = network.getContract("HomeTransfer");
 
-			result = contract.evaluateTransaction("queryHomeById", "4");
-			System.out.println(new String(result));
-			
+            byte[] result;
 
-			contract.submitTransaction("changeHomeOwnership", "4", "Joe");
+            // Retrieve transient data
+            Map<String, byte[]> transientData = getTransientData();
 
-			result = contract.evaluateTransaction("queryHomeById", "4");
-			System.out.println(new String(result));
-		}
+            // Submit the transaction using the transient data
+            contract.createTransaction("addNewHome")
+                    .setTransient(transientData)
+                    .submit("10", "Peaced", "54678", "jack Hobs", "78909");
 
+            result = contract.evaluateTransaction("queryHomeById", "10");
+            System.out.println(new String(result));
+
+            // Submit the transaction using the transient data
+       /*     contract.createTransaction("changeHomeOwnership")
+                    .setTransient(transientData)
+                    .submit("4", "Joe");  */
+
+            result = contract.evaluateTransaction("queryHomeById", "4");
+            System.out.println(new String(result));
+        }
+    }
+
+    private static Map<String, byte[]> getTransientData() {
+        Map<String, byte[]> transientData = new HashMap<>();
+        // Add transient data to control the decorators
+        transientData.put("logging", "true".getBytes());
+  
+        return transientData;
+    }
 }
-}
-	
